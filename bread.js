eec770abcaeafd721880f99a4d34794a2ce88205
Jess Brawner <jessnbillbrawner@gmail.com>
2017-09-06 16:40:48 -0500

    set up initial files

---
+++ bread.js
@@ -0,0 +1,7 @@
+var SandwichMaker = (function(maker){
+	const breads = ["Marble Rye": 0.90, "Sourdough": 0.90, "Wheat": 0.70, "Sub Roll": 0.70, "Tortilla Wrap": 0.60];
+
+	maker.getBreads = function(){
+		return breads
+	}
+})
