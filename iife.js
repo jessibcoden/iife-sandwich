da59b98a5f2f762bbc673d0439b7dc9cfec0a694
Jess Brawner <jessnbillbrawner@gmail.com>
2017-09-11 20:23:34 -0500

    completed functionality

--- iife.js
+++ iife.js
@@ -11,25 +11,40 @@ const cond = SandwichMaker.getCond();
 const meat = SandwichMaker.getMeat();
 
 let checkbox = document.getElementsByClassName("checkbox");
-let mySandwich = [];
-let myPrice = [];
-
 for(let i = 0; i < checkbox.length; i++){
 	// console.log("checkbox", checkbox[i]);
 	checkbox[i].addEventListener("click", buildSandwich);
 }
 
+let mySandwich = [];
+
+
 function buildSandwich(event){
-	console.log("event", event);
 	if(event.target.checked === true){
-		mySandwich.push(event.target.name);
-		myPrice.push(event.target.value);
+		mySandwich.push({topping: event.target.name, price: event.target.value});
 	}
-	else if(event.target.unchecked === true){
-		const index = array.indexOf(event.target);
-		array.splice(index, 1);
+	else if(event.target.checked === false){
+		for(let j = 0; j < mySandwich.length; j++){
+			console.log("mysand j", mySandwich[j]);
+			console.log("target name", event.target.name);
+			if(mySandwich[j].topping === event.target.name){
+				mySandwich.splice(j, 1);			
+			}		
+		}
 	}
-	console.log("my sandwich and price", mySandwich, myPrice);
+	let sandString = "";
+	let totalPrice = 0;
+	mySandwich.forEach((sandwich) => {
+		sandString += `<p>${sandwich.topping}</p>`;
+		totalPrice += parseFloat(sandwich.price); 
+	})
+		sandString += `<p>Total Price: $${totalPrice.toFixed(2)}</p>`;
+
+	writeToDom(sandString);	
+}
+
+let writeToDom = (strang) => {
+	document.getElementById("selected-ingredients").innerHTML = strang;
 }
 
 
